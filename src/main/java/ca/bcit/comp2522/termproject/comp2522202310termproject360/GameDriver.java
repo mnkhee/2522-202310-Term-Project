5bd7ec61e3f3c426eb5f726ea290a83199ad43d1
Joshua Chuah <113268938+mnkhee@users.noreply.github.com>
2023-03-22 22:43:12 -0700

    Renamed HelloApplication to GameDriver

--- src/main/java/ca/bcit/comp2522/termproject/comp2522202310termproject360/GameDriver.java
+++ src/main/java/ca/bcit/comp2522/termproject/comp2522202310termproject360/GameDriver.java
@@ -7,10 +7,10 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class GameDriver extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("game-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(GameDriver.class.getResource("game-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 1280, 720);
         stage.setTitle("Cookie Clicker");
         stage.setScene(scene);
