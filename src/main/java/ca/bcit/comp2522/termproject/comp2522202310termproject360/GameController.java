5fc0bc83f3d6c140f187bbd07625803ea3eeea7c
Joshua Chuah <113268938+mnkhee@users.noreply.github.com>
2023-03-29 11:41:31 -0700

    Fries implementation

--- src/main/java/ca/bcit/comp2522/termproject/comp2522202310termproject360/GameController.java
+++ src/main/java/ca/bcit/comp2522/termproject/comp2522202310termproject360/GameController.java
@@ -79,7 +79,7 @@ public class GameController {
         }
         if (player.totalRevenue() >= 1000) {
             friesUpgrade.setVisible(true);
-            friesUpgrade.setText("Fries\nPrice: $N/A");
+            friesUpgrade.setText("Fries\nPrice: $" + fries.getCost());
         }
     }
 
