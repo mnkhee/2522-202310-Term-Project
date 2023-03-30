372c5e36f2b48896d41a97a44eeb88e4a9a136ad
Joshua Chuah <113268938+mnkhee@users.noreply.github.com>
2023-03-30 10:02:42 -0700

    Create shop section

--- src/main/java/ca/bcit/comp2522/termproject/comp2522202310termproject360/Pizza.java
+++ src/main/java/ca/bcit/comp2522/termproject/comp2522202310termproject360/Pizza.java
@@ -1,4 +1,33 @@
 package ca.bcit.comp2522.termproject.comp2522202310termproject360;
 
-public class Pizza {
+public class Pizza implements Item {
+    @Override
+    public double getPassiveIncome() {
+        return 0;
+    }
+
+    @Override
+    public void setPassiveValue(double passiveValue) {
+
+    }
+
+    @Override
+    public double getCost() {
+        return 0;
+    }
+
+    @Override
+    public void setCost(double cost) {
+
+    }
+
+    @Override
+    public int getCount() {
+        return 0;
+    }
+
+    @Override
+    public void incrementCount() {
+
+    }
 }
